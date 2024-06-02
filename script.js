073c55e7c781049430cfc150da8f95749142f182
Muskan044 <muskankhan430430@gmail.com>
2024-06-02 18:28:31 +0500

    add diffferent class tasks

---
+++ script.js
@@ -0,0 +1,79 @@
+// var noOfItems = +prompt("ap ne kitni chezy li?")
+// var discount;
+
+// if (noOfItems >= 10) {
+//     discount = 20
+// } else if(noOfItems >= 5) {
+//     discount = 10
+// } else {
+//     discount = 0
+// }
+
+// console.log(`your discount is: ${discount}%`)
+
+// ATM
+// var balance = 3000;
+// var amount = +prompt("type your withdrawal amount")
+
+// if (amount <= balance){
+//     console.log("transaction succesful.")
+// } else{
+//     console.log("insufficient balance")
+// }
+
+// barish
+// var rain = prompt("badal hai?")
+ 
+// if (rain = false){
+//     console.log("barish hai")
+// } else{
+//     console.log("barish nahi h")
+// }
+
+// BRT
+// var balance = +prompt("how many balance do u have?")
+
+// if (balance >= 50){
+//     console.log("ap safar kr sakty h")
+// } else {
+//     console.log("insuficient balance")
+// }
+
+// BURGER
+// var burgerPrice = 350;
+// var amount = +prompt("apky pas kitny paise hai?")
+
+// if (amount > burgerPrice){
+//     console.log("burger le lo")
+// } else {
+//     console.log("guzara kro")
+// }
+
+// EVEN ODD
+// var number = +prompt("enter a number")
+
+// if (number % 2 === 0){
+//     console.log(`${number} is even`)
+// } else {
+//     console.log(`${number} is odd`)
+// }
+
+// TRAFFIC LIGHT
+// var userInput = prompt("enter color")
+
+// if(userInput === "red"){
+//     console.log("STOP")
+// } else if(userInput === "yellow"){
+//     console.log("GET READY")
+// } else if(userInput === "green"){
+//     console.log("GO")
+// } else {
+//     console.log("INVALID")
+// }
+
+// TOTAL BILL
+// var mango = 50;
+// var orange = 50;
+// var totalBill = mango + orange
+// console.log(totalBill)
+
